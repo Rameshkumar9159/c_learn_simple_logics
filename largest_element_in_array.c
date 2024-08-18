fde1c960d9fb85162a6b332224f9760290649382
T S RAMESHKUMAR <rameshsv06@gmail.com>
2024-08-18 10:49:23 +0530

    Add files via upload

---
+++ largest_element_in_array.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+int main(){
+
+
+	int size,temp,i=0,j;
+	printf("Enter the how many elements you need:");
+	scanf("%d\n",&size);
+	int arr[size];
+	while(i<size)
+	{
+		scanf("%d",&arr[i]);
+		i++;
+	}
+	i=0;
+	while(i<size){
+                printf("arr[%d] is %d\n",i,arr[i]);
+	        i++; 
+	}
+      for(i=0;i<size;i++){
+	      for(j=i+1;j<size;j++){
+		      if(arr[i]<arr[j]){
+			      temp=arr[i];
+			      arr[i]=arr[j];
+			      arr[j]=temp;
+		      }
+	      }
+       }
+printf("largest element in array is %d\n",arr[0]);
+
+return 0;
+}
